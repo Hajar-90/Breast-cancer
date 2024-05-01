f3120ea0da55ac6b23ef560e1a3a8af181600b84
Hajar-90 <147998132+Hajar-90@users.noreply.github.com>
2024-05-01 18:36:38 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='app.py',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'streamlit',
+        'numpy',
+        'tensorflow-cpu', 
+    ],
+)
 
