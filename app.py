1223eb49761964007b4ea3091d93df97eac63327
Hajar-90 <147998132+Hajar-90@users.noreply.github.com>
2024-05-01 21:55:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,50 +1,27 @@
-import base64
 import streamlit as st
 from keras.models import load_model
 from PIL import Image
+import numpy as np
 
-from util import classify
+from util import classify, set_background
 
-# Function to set background
-def set_background(image_file):
-    """
-    This function sets the background of a Streamlit app to an image specified by the given image file.
 
-    Parameters:
-        image_file (str): The path to the image file to be used as the background.
+set_background('/Users/hajaryahia/Downloads/breast-cancer/Hajar-app/breast-cancer/bgs/bg5.jpg')
 
-    Returns:
-        None
-    """
-    with open(image_file, "rb") as f:
-        img_data = f.read()
-    b64_encoded = base64.b64encode(img_data).decode()
-    style = f"""
-        <style>
-        .stApp {{
-            background-image: url(data:image/png;base64,{b64_encoded});
-            background-size: cover;
-        }}
-        </style>
-    """
-    st.markdown(style, unsafe_allow_html=True)
-
-# Set background
-set_background('bgs/bg5.jpg')
-
-# Set title
+# set title
 st.title('Breast Cancer classification')
 
-# Set header
+# set header
 st.header('Please upload a Breast Mammography image')
 
-# Upload file
-file = st.file_uploader('', type=['jpeg', 'jpg', 'png', 'pgm'])
+# upload file
+file = st.file_uploader('', type=['jpeg', 'jpg', 'png','pgm'])
 
-# Load classifier
-model = load_model('model-final.h5')
+# load classifier
+model = load_model('/Users/hajaryahia/Downloads/breast-cancer/model-final.h5')
+
 # load class names
-with open('labels.txt', 'r') as f:
+with open('/Users/hajaryahia/Downloads/breast-cancer/model/labels.txt', 'r') as f:
     class_names = [a[:-1].split(' ')[1] for a in f.readlines()]
     f.close()
 
@@ -54,9 +31,9 @@ if file is not None:
     st.image(image, use_column_width=True)
 
     # classify image
-    class_name, conf_score = classify(image, model, class_names)
+    class_name = classify(image, model, class_names)
 
     # write classification
     st.write("## {}".format(class_name))
-    st.write("### score: {}%".format(int(conf_score * 1000) / 10))
+
 
