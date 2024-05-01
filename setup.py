5ffcbab1051a6b339d4ba8aa1ea0b1c3dc332cdd
Hajar-90 <147998132+Hajar-90@users.noreply.github.com>
2024-05-01 18:38:37 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='app.py',
+    name='app',
     version='0.1',
     packages=find_packages(),
     install_requires=[
         'streamlit',
-        'numpy',
-        'tensorflow-cpu', 
+        'Pillow',  # This is the Python Imaging Library used for image processing with PIL
+        'tensorflow',  # Make sure to specify the version of TensorFlow you need
+        'opencv-python-headless',  # OpenCV library without GUI dependencies
+        'numpy',  # Required for numerical operations
     ],
 )
-
