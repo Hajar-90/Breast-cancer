1962dae3cdafc3827fab2a5b5263e58c6fb6e853
Hajar-90 <147998132+Hajar-90@users.noreply.github.com>
2024-05-27 21:27:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,30 +11,7 @@ set_background('bgs/bg5.jpg')
 # set title
 st.title('Breast Cancer classification')
 
-# set header
-st.header('Please upload a Breast Mammography image')
 
-# upload file
-file = st.file_uploader('', type=['jpeg', 'jpg', 'png','pgm'])
-
-# load classifier
-model = load_model('model-final.h5')
-
-# load class names
-with open('labels.txt', 'r') as f:
-    class_names = [a[:-1].split(' ')[1] for a in f.readlines()]
-    f.close()
-
-# display image
-if file is not None:
-    image = Image.open(file).convert('RGB')
-    st.image(image, use_column_width=True)
-
-    # classify image
-    class_name = classify(image, model, class_names)
-
-    # write classification
-    st.write("## {}".format(class_name))
 st.title('Breast Cancer Prediction Parameters Input')
 
 # Create text inputs for each parameter
